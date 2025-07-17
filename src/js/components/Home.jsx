3179c5432dbcda5d8e756c4bc90269283c76e2a8
Jaqueline Téllez <78996295+jaquenwn@users.noreply.github.com>
2025-07-17 20:18:20 +0000

    cambio de boton card

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 						<Card
 						title= "Card title"
 						text= "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis nihil ipsum eius sint nulla. Quis necessitatibus, voluptatem, officiis accusamus veniam, animi ipsum illo beatae dolorum reprehenderit vel nostrum? Deserunt, laudantium."
-						buttonLabel= "Go somewhere"
+						buttonLabel= "Find out more!"
 						imageUrl= "https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04141642/Free-Stock-Photos-01.jpg"
 						/>
 					</div>
@@ -31,7 +31,7 @@ const Home = () => {
 						<Card
 						title= "Card title"
 						text= "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis nihil ipsum eius sint nulla. Quis necessitatibus, voluptatem, officiis accusamus veniam, animi ipsum illo beatae dolorum reprehenderit vel nostrum? Deserunt, laudantium."
-						buttonLabel= "Go somewhere"
+						buttonLabel= "Find out more!"
 						imageUrl= "https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=612x612&w=0&k=20&c=i4HYO7xhao7CkGy7Zc_8XSNX_iqG0vAwNsrH1ERmw2Q="
 						/>
 					</div>
@@ -39,7 +39,7 @@ const Home = () => {
 						<Card
 						title= "Card title"
 						text= "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis nihil ipsum eius sint nulla. Quis necessitatibus, voluptatem, officiis accusamus veniam, animi ipsum illo beatae dolorum reprehenderit vel nostrum? Deserunt, laudantium."
-						buttonLabel= "Go somewhere"
+						buttonLabel= "Find out more!"
 						imageUrl= "https://s1.1zoom.me/prev/587/Waves_Closeup_Water_586392_612x400.jpg"
 						/>
 					</div>
@@ -47,7 +47,7 @@ const Home = () => {
 						<Card
 						title= "Card title"
 						text= "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis nihil ipsum eius sint nulla. Quis necessitatibus, voluptatem, officiis accusamus veniam, animi ipsum illo beatae dolorum reprehenderit vel nostrum? Deserunt, laudantium."
-						buttonLabel= "Go somewhere"
+						buttonLabel= "Find out more!"
 						imageUrl= "https://media.istockphoto.com/id/901134626/photo/sun-shining-in-a-forest.jpg?s=612x612&w=0&k=20&c=Z1GPER9EE6BQK_efVKaSO7WODScQYOMPX3TFFPCZt88="
 						/>
 					</div>
