7c4ce6527af73c2fcf04771755effc890896af2e
Jaqueline Téllez <78996295+jaquenwn@users.noreply.github.com>
2025-07-17 15:22:30 +0000

    componentes basico

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import Card from './Card';
+import Navbar from './Navbar';
+import Jumbotron from './Jumbotron';
 //create your first component
 const Home = () => {
 	return (
